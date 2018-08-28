09984a49ffbc93fd412de719fcacd06919efe31b
turbotage <viktor.vigrennaslund@live.se>
2018-08-28 15:56:22 +0200

    redo of last commit

--- webserver.go
+++ webserver.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -22,18 +23,82 @@ type AddWinnerRequest struct {
 
 var xlChallenges *excelize.File
 var challengeSheet string
+var nRowsC int
+var nColsC int
+
+var xlPasswords *excelize.File
+var passwordSheet string
+var nRowsP int
+var nColsP int
+
+type AddWinnerRequest struct {
+	ChallengeID int    `json:"challenge_id"`
+	NollaName   string `json:"nolla"`
+	Password    string `json:"password"`
+}
+
+type TableRowResponseData struct {
+	CID     int    `json:"cid"`
+	CName   string `json:"cname"`
+	Points  int    `json:"points"`
+	Nolla   string `json:"nolla"`
+	Phadder string `json:"phadder"`
+}
 
 func getChallenge(challengeID int) string {
 	return xlChallenges.GetCellValue(challengeSheet, "A"+strconv.Itoa(challengeID+1))
 }
 
 func getNolla(challengeID int) string {
-	return xlChallenges.GetCellValue(challengeSheet, "E"+strconv.Itoa(challengeID+1))
+	return xlChallenges.GetCellValue(challengeSheet, "C"+strconv.Itoa(challengeID+1))
+}
+
+func getTable() ([]TableRowResponseData, int) {
+	tableRowResponse := make([]TableRowResponseData, nRowsC-1)
+	length := 0
+	rows := xlChallenges.GetRows(challengeSheet)
+	for i := 1; i < nRowsC; i++ {
+		tableRowResponse[i-1].CID = i
+		tableRowResponse[i-1].CName = rows[i][0]
+		tableRowResponse[i-1].Points, _ = strconv.Atoi(rows[i][1])
+		tableRowResponse[i-1].Nolla = rows[i][2]
+		tableRowResponse[i-1].Phadder = rows[i][3]
+		length = i
+	}
+	return tableRowResponse, length
+}
+
+func findPassword(password string) int {
+	for i := 1; i <= nRowsP; i++ {
+		if password == xlPasswords.GetCellValue(passwordSheet, "B"+strconv.Itoa(i)) {
+			return i
+		}
+	}
+	return -1
+}
+
+func getPhadderFromPass(num int) string {
+	return xlPasswords.GetCellValue(passwordSheet, "A"+strconv.Itoa(num))
+}
+
+func setNolla(num int, str string) {
+	xlChallenges.SetCellValue(challengeSheet, "C"+strconv.Itoa(num+1), str)
+}
+
+func setPhadder(num int, str string) {
+	xlChallenges.SetCellValue(challengeSheet, "D"+strconv.Itoa(num+1), str)
 }
 
 func main() {
 
 	challengeSheet = "Blad1"
+	nRowsC = 52
+	nColsC = 5
+
+	passwordSheet = "Blad1"
+	nRowsP = 25
+	nColsP = 2
+
 	//var ip = flag.String("database_ip", "127.0.0.1", "the ip to the database")
 	//var port = flag.String("database_port", "5555", "the port to the database")
 	//var user = flag.String("database_username", "turbotage", "the username to the database")
@@ -45,6 +110,7 @@ func main() {
 	var err error
 
 	xlChallenges, err = excelize.OpenFile("./Utmaningar.xlsx")
+	xlPasswords, err = excelize.OpenFile("./Passwords.xlsx")
 
 	if err != nil {
 		fmt.Println(err)
@@ -61,21 +127,53 @@ func main() {
 		log.Println("New client connected")
 		//join them to room
 		c.Join("chat")
+		c.Emit("update", "jas")
 	})
 
-	type AddWinnerRequest struct {
-		ChallengeID int    `json:"challenge_id"`
-		NollaName   string `json:"nolla"`
-		Password    string `json:"password"`
-	}
+	server.On("table-req", func(c *gosocketio.Channel, num int) string {
+		arr, _ := getTable()
+		b, err := json.Marshal(arr)
+		if err != nil {
+			log.Println(err)
+		}
+		c.Emit("table-response", string(b))
+		return "OK"
+	})
 
 	//handle custom event
 	server.On("add-winner", func(c *gosocketio.Channel, winReq AddWinnerRequest) string {
 		//send event to all in room
 
-		log.Println(winReq.ChallengeID)
-		log.Println(winReq.NollaName)
-		log.Println(winReq.Password)
+		challengeID := winReq.ChallengeID
+
+		if (challengeID < 1) || (challengeID > (nRowsC - 1)) {
+			c.Emit("alert", "Det specifierade Utmanings ID har ingen matchande utmaning")
+			return "ERROR"
+		}
+
+		nolla := getNolla(challengeID)
+		if len(nolla) != 0 {
+			c.Emit("alert", nolla+" har redan klarat denna utmaning")
+			return "ERROR"
+		}
+		if len(winReq.NollaName) > 50 {
+			c.Emit("alert", "nollegruppsnamn är för långt")
+			return "ERROR"
+		}
+
+		num := findPassword(winReq.Password)
+		if num == -1 {
+			c.Emit("alert", "inget matchande lösenord")
+			return "ERROR"
+		}
+		phadder := getPhadderFromPass(num)
+		setNolla(challengeID, winReq.NollaName)
+		setPhadder(challengeID, phadder)
+
+		xlChallenges.Save()
+
+		c.Emit("update", "added winner")
+
 		return "OK"
 	})
 
